58a7774fafb84681596a35ad52f0c21b6b3f2a95
MiqueiasAmorimm <miqueias202201@hotmail.com>
2026-03-10 01:57:45 -0300

    chore: configure PostgreSQL datasource and environment variables

--- src/main/java/com/pettime/controller/TestController.java
+++ src/main/java/com/pettime/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.pettime.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class TestController {
 
     @GetMapping("/test")
